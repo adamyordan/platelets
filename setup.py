41da8c715d56dacf967e049b7ab0b603d62b9dc9
Adam Jordan <adamyordan@gmail.com>
2019-06-21 01:58:47 +0800

    add sound; drop python2 support

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import setuptools
+from pip._internal.req import parse_requirements
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
+
 setuptools.setup(
     name='platelets',  
-    version='0.2',
-    scripts=['python3/platelets'] ,
+    version='0.4',
+    scripts=['platelets/platelets'] ,
     author="Adam Jordan",
     author_email="adamyordan@gmail.com",
     description="Supply your daily platelets intake directly from terminal",
@@ -18,5 +26,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    install_requires=load_requirements('requirements.txt'),
 )
